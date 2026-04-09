b62e61bc1e2250839bbe7d8fd92fed1a0630c5f7
Thom Mott <thommott@proton.me>
2026-04-08 22:25:54 -0700

    samba

--- modules/hosts/tink/configuration.nix
+++ modules/hosts/tink/configuration.nix
@@ -68,6 +68,57 @@
           auto-cpufreq.enable = false;
           tlp.enable = false;
           displayManager.defaultSession = "gnome";
+
+          samba = {
+            enable = true;
+            securityType = "user";
+            openFirewall = true;
+            settings = {
+              global = {
+                "workgroup" = "WORKGROUP";
+                "server string" = "smbnix";
+                "netbios name" = "smbnix";
+                "security" = "user";
+                #"use sendfile" = "yes";
+                #"max protocol" = "smb2";
+                # note: localhost is the ipv6 localhost ::1
+                "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+                "hosts deny" = "0.0.0.0/0";
+                "guest account" = "nobody";
+                "map to guest" = "bad user";
+                "fruit:aapl" = "yes";
+                "vfs objects" = "catia fruit streams_xattr";
+              };
+              "public" = {
+                "path" = "/mnt/Shares/Public";
+                "browseable" = "yes";
+                "read only" = "no";
+                "guest ok" = "yes";
+                "create mask" = "0644";
+                "directory mask" = "0755";
+                "force user" = "username";
+                "force group" = "groupname";
+              };
+              "private" = {
+                "path" = "/mnt/Shares/Private";
+                "browseable" = "yes";
+                "read only" = "no";
+                "guest ok" = "no";
+                "create mask" = "0644";
+                "directory mask" = "0755";
+                "force user" = "username";
+                "force group" = "groupname";
+              };
+              "tm_share" = {
+                "path" = "/mnt/Shares/tm_share";
+                "valid users" = "username";
+                "public" = "no";
+                "writeable" = "yes";
+                "force user" = "username";
+                "fruit:time machine" = "yes";
+              };
+            };
+          };
         };
 
         my.login.sddm = {
